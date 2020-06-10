4163ab7e15aa63eccbddd83df76eb93382dc03c1
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-06-10 23:00:20 +0530

    added some sql queries in backend

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  database: 'interview_bit',
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  
+});
 router.get('/list', function(req, res, next) {
-  res.send('respond with a resource');
+  const myQuery="SELECT * FROM day"
+  con.query(myQuery, function (err, result, fields) {
+		if (err){
+			res.send('an error occoured');
+			throw err;
+		}
+		console.log(result);
+		res.send(result);
+	});
 });
 router.post('/add', function(req, res, next) {
-  res.send('respond with a resource');
+  const myQuery="INSERT INTO day (number,date,link,topic,sub_topic_count) VALUES ("
+                  +req.body.number
+                  +req.body.date
+                  +req.body.link
+                  +req.body.topic
+                  +req.body.sub_topic_count
+                  +");"
+  con.query(myQuery, function (err, result, fields) {
+		if (err){
+			res.send('an error occoured');
+			throw err;
+		}
+		console.log(result);
+		res.send(result);
+	});
 });
 router.put('/data', function(req, res, next) {
   res.send('respond with a resource');
